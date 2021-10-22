d1127d67d1f4804d81f9bbc069c4f196aeebd697
-pragati- <-lendgharp117@gmail.com->
2021-10-22 22:13:34 +0530

    lastnewfinh

---
+++ src/test/java/test/newclass.java
@@ -0,0 +1,12 @@
+package test;
+
+public class newclass {
+int i=10;
+int j=20;
+int c=i+j;
+private void Syso() {
+	// TODO Auto-generated method stub
+	System.out.println("test1");
+
+}
+}
--- src/test/java/test/test12.java
+++ src/test/java/test/test12.java
@@ -8,7 +8,17 @@ System.out.println("this is git learing ");
 System.out.println("Doing checking all the the command on Git ");
 System.out.println("Usung Github");
 System.out.println("test");
+{
+	
+
 
 	}
 
 }
+
+public void test() {
+	System.out.println("first test");
+	
+}
+}
+
