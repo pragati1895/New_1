a5b7c89df8ff2ee283d5681a818b249ea78dde0f
-pragati- <-lendgharp117@gmail.com->
2021-10-22 21:10:04 +0530

    tesr1

---
+++ src/test/java/test/test12.java
@@ -0,0 +1,14 @@
+package test;
+
+public class test12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is git learing ");
+System.out.println("Doing checking all the the command on Git ");
+System.out.println("Usung Github");
+
+
+	}
+
+}
